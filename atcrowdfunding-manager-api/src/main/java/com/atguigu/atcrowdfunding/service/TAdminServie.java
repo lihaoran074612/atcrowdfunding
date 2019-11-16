dee233703d17a26442dbca16da952a05f8d503d5
李浩燃 <577979102@qq.com>
2019-11-16 23:07:57 +0800

    TAdmin

--- atcrowdfunding-manager-api/src/main/java/com/atguigu/atcrowdfunding/service/TAdminServie.java
+++ atcrowdfunding-manager-api/src/main/java/com/atguigu/atcrowdfunding/service/TAdminServie.java
@@ -3,9 +3,11 @@ package com.atguigu.atcrowdfunding.service;
 import java.util.Map;
 
 import com.atguigu.atcrowdfunding.bean.TAdmin;
+import com.github.pagehelper.PageInfo;
 
 public interface TAdminServie {
 
 	TAdmin getTAdminByLogin(Map<String, String> paramMap);
 
+    PageInfo<TAdmin> listAdminPage();
 }
--- atcrowdfunding-manager-impl/src/main/java/com/atguigu/atcrowdfunding/controller/TAdminController.java
+++ atcrowdfunding-manager-impl/src/main/java/com/atguigu/atcrowdfunding/controller/TAdminController.java
@@ -1,12 +1,33 @@
 package com.atguigu.atcrowdfunding.controller;
 
+import com.atguigu.atcrowdfunding.bean.TAdmin;
+import com.atguigu.atcrowdfunding.service.TAdminServie;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class TAdminController {
+    @Autowired
+    TAdminServie adminServie;
+
     @RequestMapping("/admin/index")
-    public String index(){
+    public String index(@RequestParam(value = "pageNum",required = false,defaultValue = "1") int pageNum,
+                        @RequestParam(value = "pageSize",required = false,defaultValue = "1") int pageSize,
+                        Model model){
+        PageHelper.startPage(pageNum,pageSize);
+        Map<String,Object> map = new HashMap<>();
+        PageInfo<TAdmin> pageInfo = adminServie.listAdminPage();
+        model.addAttribute("page",pageInfo);
         return "admin/index";
     }
+
+
 }
